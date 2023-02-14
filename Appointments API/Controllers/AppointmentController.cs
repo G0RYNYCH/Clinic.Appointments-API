95dcedc7464694821bf29684fb78349a5c5fad4a
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2023-02-14 10:08:05 +0300

    add HttpContext.RequestAborted

--- Appointments API/Controllers/AppointmentController.cs
+++ Appointments API/Controllers/AppointmentController.cs
@@ -16,41 +16,41 @@ public class AppointmentController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAppointments([FromQuery] SearchDto searchDto, CancellationToken cancellationToken) //TODO: [FromBody]
+    public async Task<IActionResult> GetAppointments([FromQuery] SearchDto searchDto)
     {
-        var paginatedAppointments = await _appointmentService.SearchAsync(searchDto, cancellationToken);
+        var paginatedAppointments = await _appointmentService.SearchAsync(searchDto, HttpContext.RequestAborted);
 
         return Ok(paginatedAppointments);
     }
 
-    [HttpGet("{id:guid}")] //TODO: [HttpGet("{id:guid}")]
-    public async Task<IActionResult> GetById([FromQuery]  Guid id, CancellationToken cancellationToken)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
-        var appoitment = await _appointmentService.GetByIdAsync(id, cancellationToken);
+        var appoitment = await _appointmentService.GetByIdAsync(id, HttpContext.RequestAborted);
 
         return Ok(appoitment);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] AppointmentDto appointmentDto, CancellationToken cancellationToken)
+    public async Task<IActionResult> Create([FromBody] AppointmentDto appointmentDto)
     {
-        await _appointmentService.CreateAsync(appointmentDto, cancellationToken);
+        await _appointmentService.CreateAsync(appointmentDto, HttpContext.RequestAborted);
 
-        return Ok();//TODO:
+        return Ok();
     }
 
     [HttpPut("{id:guid}")] //TODO: HttpPatch
-    public async Task<IActionResult> Update(Guid id, UpdateAppointmentDto updateAppointmentDto, CancellationToken cancellationToken)
+    public async Task<IActionResult> Update(Guid id, UpdateAppointmentDto updateAppointmentDto)
     {
-        await _appointmentService.UpdateAsync(id, updateAppointmentDto, cancellationToken);
+        await _appointmentService.UpdateAsync(id, updateAppointmentDto, HttpContext.RequestAborted);
 
         return Ok();
     }
 
     [HttpDelete("{id:guid}")]
-    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    public async Task<IActionResult> Delete(Guid id)
     {
-        await _appointmentService.DeleteAsync(id, cancellationToken);
+        await _appointmentService.DeleteAsync(id, HttpContext.RequestAborted);
 
         return NoContent();
     }
