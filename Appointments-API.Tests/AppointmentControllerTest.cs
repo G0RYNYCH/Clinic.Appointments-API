86e189c66fb3afd64c57da725859a76b239435c8
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2023-02-17 16:11:18 +0300

    started unit testing

---
+++ Appointments-API.Tests/AppointmentControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Appointments_API.Controllers;
+using Appointments_API.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using Xunit;
+
+namespace Appointments_API.Tests;
+
+public class AppointmentControllerTest : IClassFixture<AppointmentController>
+{
+    private readonly AppointmentController _appointmentController;
+    private readonly Mock<IAppointmentService> _appointmentService;
+    private readonly Mock<ILogger<AppointmentController>> _logger;
+
+    public AppointmentControllerTest(AppointmentController appointmentController, 
+        Mock<IAppointmentService> appointmentService, Mock<ILogger> logger)
+    {
+        _appointmentController = new AppointmentController(_appointmentService.Object, _logger.Object);
+        _appointmentService = new Mock<IAppointmentService>();
+        _logger = new Mock<ILogger<AppointmentController>>();
+    }
+
+    [Fact]
+    public void AppointmentControllerCtor_WithNullAppointmentService_ThrowsNullArgumentExcception()
+    {
+        Assert.Throws<ArgumentNullException>(() => new AppointmentController(null, _logger.Object));
+    }
+
+    [Fact]
+    public void AppointmentControllerCtor_WithNullLogger_ThrowsNullArgumentExcception()
+    {
+        Assert.Throws<ArgumentNullException>(() => new AppointmentController(_appointmentService.Object, null));
+    }
+
+    [Fact]
+    public void AppointmentControllerCtor_WithNotNullArgs_CreatesInstance()
+    {
+        Assert.Throws<ArgumentNullException>(() => new AppointmentController(_appointmentService.Object, _logger.Object));
+    }
+}
