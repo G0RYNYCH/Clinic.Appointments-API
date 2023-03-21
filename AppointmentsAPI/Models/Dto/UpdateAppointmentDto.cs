d593d14aa1d8a18809d5af94bd04463fcda5e41b
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2023-03-21 17:21:49 +0300

    add typed httpClient

--- AppointmentsAPI/Models/Dto/UpdateAppointmentDto.cs
+++ AppointmentsAPI/Models/Dto/UpdateAppointmentDto.cs
@@ -5,15 +5,15 @@ namespace AppointmentsAPI.Models.Dto;
 [ExcludeFromCodeCoverage]
 public class UpdateAppointmentDto
 {
-    public Guid? PatientId { get; set; }
+    public Guid PatientId { get; set; }
 
-    public Guid? DoctorId { get; set; }
+    public Guid DoctorId { get; set; }
 
-    public Guid? ServiceId { get; set; }
+    public Guid ServiceId { get; set; }
 
-    public DateOnly? Date { get; set; }
+    public DateOnly Date { get; set; }
 
-    public TimeOnly? Time { get; set; }
+    public TimeOnly Time { get; set; }
 
-    public bool? IsApproved { get; set; }
+    public bool IsApproved { get; set; }
 }
